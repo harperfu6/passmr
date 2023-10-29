016edc8e2b1986976c5135f14ea7f9c1cb0c9cc9
harperfu6 <beharp8a8@gmail.com>
2023-10-29 19:40:36 +0900

    add input

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,57 @@
 use std::{error::Error, io};
 
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event},
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use ratatui::prelude::*;
 use ratatui::widgets::*;
 
-struct App {}
+/// holds the state of the application
+struct App {
+    /// current value of the input
+    input: String,
+    /// cursor position in the input
+    cursor_position: usize,
+}
 
-impl App {}
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            input: String::new(),
+            cursor_position: 0,
+        }
+    }
+}
 
-fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
+impl App {
+    fn move_cursor_right(&mut self) {
+        let cursor_moved_right = self.cursor_position.saturating_add(1);
+        self.cursor_position = self.clamp_cursor_position(cursor_moved_right);
+    }
+
+    fn clamp_cursor_position(&self, new_cursor_position: usize) -> usize {
+        new_cursor_position.clamp(0, self.input.len())
+    }
+
+    fn enter_char(&mut self, c: char) {
+        self.input.insert(self.cursor_position, c);
+        self.move_cursor_right();
+    }
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
     loop {
-        terminal.draw(|frame| ui(frame))?;
+        terminal.draw(|frame| ui(frame, &app))?;
 
         if let Event::Key(key) = event::read()? {
             match key.code {
-                crossterm::event::KeyCode::Char('q') => {
+                KeyCode::Char('q') => {
                     return Ok(());
+                }
+                KeyCode::Char(to_insert) => {
+                    app.enter_char(to_insert);
                 }
                 _ => {}
             }
@@ -27,14 +59,37 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
     }
 }
 
-fn ui(frame: &mut Frame) {
+fn ui(frame: &mut Frame, app: &App) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Length(1),
+            Constraint::Length(3),
+            Constraint::Min(3),
+        ])
+        .split(frame.size());
+
     let mut text = Text::from(Line::from("Hello World!"));
     let style = Style::default()
         .fg(Color::Yellow)
         .add_modifier(Modifier::ITALIC);
     text.patch_style(style);
     let message = Paragraph::new(text);
-    frame.render_widget(message, Rect::new(0, 0, 20, 20));
+    frame.render_widget(message, chunks[0]);
+
+    let search = Paragraph::new(app.input.as_str())
+        .style(Style::default().fg(Color::White))
+        .block(Block::default().borders(Borders::ALL).title("Search"));
+    frame.render_widget(search, chunks[1]);
+    frame.set_cursor(
+        chunks[1].x + app.cursor_position as u16 + 1,
+        chunks[1].y + 1,
+    );
+
+    let list = Paragraph::new("")
+        .style(Style::default().fg(Color::White))
+        .block(Block::default().borders(Borders::ALL).title("List"));
+    frame.render_widget(list, chunks[2]);
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -45,7 +100,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    run_app(&mut terminal)?;
+    let app = App::default();
+    run_app(&mut terminal, app)?;
 
     // restore terminal
     disable_raw_mode()?;
